9d867c50c77e92095468632b716a3246fa455062
Pat <dmtpatricica@email.com>
2026-02-25 21:34:20 +0100

    destroy function

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,10 @@
 const posts = require("../public/posts.js")
 
+//--------------------------------------------------------------------
 function index(req, res) {
     res.json(posts);
 }
+//--------------------------------------------------------------------
 function show(req, res) {
     const selectedPost = posts.find(post => post.id == parseInt(req.params.id))
     res.send(selectedPost);
@@ -16,8 +18,22 @@ function update(req, res) {
 function modify(req, res) {
     res.send(`Vuoi aggiornare (parzialmente) il post numero: ${req.params.id}`);
 }
+//--------------------------------------------------------------------
 function destroy(req, res) {
-    res.send(`Vuoi eliminare il post numero: ${req.params.id}`);
+    const id = Number(req.params.id)
+
+    if (isNaN(id)) {
+        return res.status(400).json({ error: "User Error", message: "Id non valido" })
+    }
+    const result = posts.find(post => post.id == id)
+
+    if (!result) {
+        return res.status(404).json({ error: "Not Found", message: "Post non trovato" })
+    }
+    posts.splice(posts.indexOf(result), 1);
+    console.log(`pizza ${id} eliminata`, posts)
+    return res.sendStatus(204);
+
 }
 
 
